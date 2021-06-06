fad149c6d827226cafb4ac45148fabf854941daf
Augustob790 <augustosousa790@gmail.com>
2021-06-06 18:54:44 -0300

    Aula 4

---
+++ lib/challenge/challenge_page.dart
@@ -0,0 +1,46 @@
+import 'package:DevQuiz/challenge/widget/next_button/next_button_widget.dart';
+import 'package:DevQuiz/challenge/widget/question_indicator/question_indicator_widget.dart';
+import 'package:DevQuiz/challenge/widget/quiz/quiz_widget.dart';
+import 'package:flutter/material.dart';
+
+class ChallangePage extends StatefulWidget {
+  @override
+  _ChallangePageState createState() => _ChallangePageState();
+}
+
+class _ChallangePageState extends State<ChallangePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(60),
+        child: SafeArea(
+          top: true,
+          child: QuestionIndicatorWidget(),
+        ),
+      ),
+      body: QuizWidget(
+        title: "O que o Flutter faz em sua totalidade?",
+      ),
+      bottomNavigationBar: SafeArea(
+        bottom: true,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Expanded(
+                  child: NextButtonWidget.white(label: "Fácil", onTap: () {})),
+              SizedBox(
+                width: 7,
+              ),
+              Expanded(
+                  child:
+                      NextButtonWidget.green(label: "Confirmar", onTap: () {})),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
